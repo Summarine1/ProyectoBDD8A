c9bdcb5868fd08f32cedd41553d318f92f60f77e
Victor Hugo Olivetti Alvarez <victoralvarez.vhoa@gmail.com>
2021-06-17 15:05:38 -0500

    Cambios de BD

--- Backend/connections/MySQLPoolLinux.js
+++ Backend/connections/MySQLPoolLinux.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const MySQLPoolLinux = mysql.createPool({
-	host: '172.23.101.143',
+	host: '172.23.25.96',
 	user: 'lnxarchitect',
 	password: '0l1v3tt1_@lv@r32_lnxarchitect',
 	database: 'acmelnx'
